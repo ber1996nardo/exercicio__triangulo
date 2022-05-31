d3cfe6a5d8292628dfeb50244f850a1da8c4349c
ber1996nardo <ninebernardo@hotmail.com>
2022-05-31 16:55:57 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio__triangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿int l1 = 7;
+int l2 = 7;
+int l3 = 7;
+
+
+if (l1 + l2 >= l3 && l1 + l3 >= l2 && l2 + l3 >= l1)
+{
+    if (l1 == l2 && l2 == l3 && l3 == l1)
+    {
+        Console.WriteLine("Triangulo Equilatero");
+    }
+    else if (l1 == l2 || l2 == l3 || l3 == l1)
+    {
+        Console.WriteLine("Triangulo Isosceles");
+    }
+    else if (l1 != l2 && l2 != l3 && l3 != l1)
+    {
+        Console.WriteLine("Triangulo Escaleno");
+    }
+}
+else
+    Console.WriteLine("Valores invalidos para formar um triangulo");
+
